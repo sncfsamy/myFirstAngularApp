c40ed52f7502bda09807428f0e8ced0bf98a9d1c
sncfsamy <106074922+sncfsamy@users.noreply.github.com>
2023-03-28 11:36:49 +0200

    done

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import User from './User';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent {
+  ageShowed = false;
+  user: User = new User('Doe', 'John',25,'two beer or not to beer','https://randomuser.me/api/portraits/lego/2.jpg');
+  toggleAgeShowed() {
+    this.ageShowed = !this.ageShowed;
+  }
+}
