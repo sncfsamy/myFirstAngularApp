6c9420dc1cc254a47dab5edb8d2e60a1f76fcfd7
sncfsamy <106074922+sncfsamy@users.noreply.github.com>
2023-03-31 11:12:57 +0200

    Revert "wip"
    This reverts commit 970fdac6ee3412c867587db3dcd74c154da03a58.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
