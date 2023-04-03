b927f8b046054c538a7978be4d283080f32ce42a
sncfsamy <106074922+sncfsamy@users.noreply.github.com>
2023-04-03 10:49:42 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
